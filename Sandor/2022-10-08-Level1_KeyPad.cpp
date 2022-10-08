2a05e4fcf08359dc715413b5abe87f8129fa2071
Sandor2889 <sandor2889@naver.com>
2022-10-08 21:11:59 +0900

    주석 깨짐 수정

--- Sandor/2022-10-08-Level1_KeyPad.cpp
+++ Sandor/2022-10-08-Level1_KeyPad.cpp
@@ -8,7 +8,7 @@ string solution(vector<int> numbers, string hand)
 {
     string answer = "";
 
-    // �޼հ� ������ �ʱ���ġ <x,y>
+    // 왼손과 오른손 초기 위치 <x,y>
     pair<int, int> lPos;
     lPos.first = 0;
     lPos.second = 3;
@@ -18,7 +18,7 @@ string solution(vector<int> numbers, string hand)
 
     for (int i = 0; i < numbers.size(); i++)
     {
-        // 0 �� ���� ó��
+        // 0 에 대한 처리
         if (numbers[i] == 0) { numbers[i] = 11; }
 
         if (numbers[i] % 3 == 1) // 1, 4, 7
@@ -35,7 +35,7 @@ string solution(vector<int> numbers, string hand)
         }
         else  // 2, 5, 8, 0
         {
-            // Ÿ�����κ��� �޼հ� ������ �Ÿ� ��
+            // 타겟으로부터 왼손과 오른손 거리 비교
             int targetX = 1;
             int targetY = numbers[i] / 3;
             int lDist = (targetX - lPos.first) + abs(targetY - lPos.second);
@@ -53,7 +53,7 @@ string solution(vector<int> numbers, string hand)
                 lPos.first = targetX;
                 lPos.second = targetY;
             }
-            else // ��ġ ���� ���� ���
+            else // 위치 값이 같을 경우
             {
                 if (hand == "left")
                 {
@@ -72,22 +72,4 @@ string solution(vector<int> numbers, string hand)
     }
 
     return answer;
-}
-
-int main()
-{
-    vector<int> numbers;
-    string hand = "left";
-    
-    for (int i = 0; i < 11; i++)
-    {
-        int temp;
-        cin >> temp;
-        numbers.push_back(temp);
-    }
-
-    string z = solution(numbers, hand);
-    cout << z;
-
-    return 0;
 }
