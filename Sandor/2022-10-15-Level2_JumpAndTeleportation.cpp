66f164227c450871969b82a04cd038c392e33153
Sandor2889 <sandor2889@naver.com>
2022-10-18 00:13:17 +0900

    백준 문제 완료

--- Sandor/2022-10-15-Level2_JumpAndTeleportation.cpp
+++ Sandor/2022-10-15-Level2_JumpAndTeleportation.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// 2�� �ŵ����� �Ǻ�
+// 2의 거듭제곱 판별
 bool isTwoPower(int n)
 {
     int temp = 1;
@@ -15,7 +15,7 @@ bool isTwoPower(int n)
     }
 }
 
-// n�� 2�� �ŵ� �����϶� ���� ���� Ƚ���� �̵� �����ϴ�
+// n이 2의 거듭 제곱일때 가장 적은 횟수로 이동 가능하다
 int solution(int n)
 {
     int ans = 1;
@@ -24,7 +24,7 @@ int solution(int n)
     {
         while (n > 1)
         {
-            if (n % 2 == 1) { ans++; }   // ������ �������� ������ �����ؾ� ��
+            if (n % 2 == 1) { ans++; }   // 나누어 떨어지지 않으면 점프해야 됨
             n /= 2;
         }
     }
---
+++ Sandor/2022-10-17-1260_DFSAndBFS.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <memory.h>
+#include <queue>
+
+#define MAX 1001
+
+using namespace std;
+
+// �Է� ���� : ���� ��(n), ���� ��(m), ���� ����(v)
+int n, m, v;
+vector<int> graph[MAX];
+bool visited[MAX];
+
+void bfs(int start)
+{
+    queue<int> q;
+    q.push(start);
+    visited[start] = true;
+    cout << start << ' ';
+
+    while (!q.empty())
+    {
+        int x = q.front();
+        q.pop();
+        
+        // 다음 노드 방문 확인
+        for (int i = 0; i < graph[x].size(); i++)
+        {
+            int next = graph[x][i];
+
+            if (!visited[next])
+            {
+                q.push(next);
+                visited[next] = true;
+                cout << next << ' ';
+            }
+        }
+    }
+}
+
+void dfs(int start)
+{
+    visited[start] = true;
+    cout << start << ' ';
+
+    // 다음 노드 방문 확인
+    for (int i = 0; i < graph[start].size(); i++)
+    {
+        int next = graph[start][i];
+        if (!visited[next])
+        {
+            dfs(next);
+        }
+    }
+}
+
+int main()
+{ 
+    cin >> n >> m >> v;
+
+    // 간선 연결
+    for (int i = 0; i < m; i++)
+    {
+        int from, to;
+        cin >> from >> to;
+       
+        // 양방향 연결
+        graph[from].push_back(to);
+        graph[to].push_back(from);
+    }
+
+    // 작은 수 먼저 방문을 위한 정렬
+    for (int i = 0; i < MAX; i++)
+    {
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    // dfs 실행
+    dfs(v);
+
+    // 방문 처리 초기화
+    cout << '\n';
+    memset(visited, false, sizeof(visited));
+
+    // bfs 실행
+    bfs(v);
+
+
+    return 0;
+}
