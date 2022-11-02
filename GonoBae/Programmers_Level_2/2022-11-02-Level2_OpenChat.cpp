afeb524b6ba927091aebfb5ab14586d57e93d1c3
GonoBae <qldrhqorhsh@naver.com>
2022-11-03 00:13:09 +0900

    풀이 추가

--- GonoBae/Programmers_Level_2/2022-11-02-Level2_OpenChat.cpp
+++ GonoBae/Programmers_Level_2/2022-11-02-Level2_OpenChat.cpp
@@ -2,8 +2,11 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <sstream>
+
 using namespace std;
 
+// 내 풀이
 vector<string> solution(vector<string> record) {
     vector<string> answer;
     unordered_map<string, string> user;
@@ -24,6 +27,8 @@ vector<string> solution(vector<string> record) {
         if(act[i][0] == "Leave") continue;
         user[act[i][1]] = act[i][2];
     }
+    
+
     // Enter , Leave , Change 구분해서 기능구현
     for(int i = 0; i < act.size(); ++i) {
         string cmd = act[i][0];
@@ -38,12 +43,42 @@ vector<string> solution(vector<string> record) {
     return answer;
 }
 
+// 참고
+vector<string> solutionB(vector<string> record) {
+    vector<string> answer;
+    unordered_map<string, string> user;
+    string cmd , id , nick;
+    for(auto r : record) {
+        stringstream ss(r);
+        ss >> cmd;
+        ss >> id;
+        if(cmd == "Leave") continue;
+        ss >> nick;
+        user[id] = nick;
+    }
+
+    for(auto r : record) {
+        stringstream ss(r);
+        ss >> cmd;
+        ss >> id;
+        if(cmd == "Enter") {
+            string str = user[id] + "님이 들어왔습니다.";
+            answer.push_back(str);
+        }
+        else if(cmd == "Leave") {
+            string str = user[id] + "님이 나갔습니다.";
+            answer.push_back(str);
+        }
+    }
+    return answer;
+}
+
 int main() {
     vector<string> record = {
-        "Enter uid1234123 Muzi",
+        "Enter uid1234 Muzi",
         "Enter uid4567 Prodo",
-        "Leave uid1234123",
-        "Enter uid1234123 Prodo",
+        "Leave uid1234",
+        "Enter uid1234 Prodo",
         "Change uid4567 Ryan"
         };
     vector<string> result = solution(record);
