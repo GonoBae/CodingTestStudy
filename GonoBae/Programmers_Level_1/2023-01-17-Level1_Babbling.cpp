5d5e41f4f3106722decebabd1c93fb9d17850409
GonoBae <qldrhqorhsh@naver.com>
2023-01-18 00:14:24 +0900

    Modi

--- GonoBae/Programmers_Level_1/2023-01-17-Level1_Babbling.cpp
+++ GonoBae/Programmers_Level_1/2023-01-17-Level1_Babbling.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 // 연속 발음 X
-int solution(vector<string> babbling) {
+int solutionA(vector<string> babbling) {
     int answer = 0;
     string prev = "";
     // ye(2), ma(2)
@@ -26,6 +26,26 @@ int solution(vector<string> babbling) {
         }
         if(check) ++answer;
         prev = "";
+    }
+    return answer;
+}
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    // ye(2), ma(2)
+    // aya(3), woo(3)
+    for(auto b : babbling) {
+        string temp = "", prev = "";
+        for(int i = 0; i < b.size(); ++i) {
+            temp += b[i];
+            if(temp.size() < 2) continue;
+            if((temp == "ye" || temp == "ma" || temp == "aya" || temp == "woo") && temp != prev) {
+                prev = temp;
+                temp = "";
+                continue;
+            }
+        }
+        if(temp.size() == 0) ++answer;
     }
     return answer;
 }
