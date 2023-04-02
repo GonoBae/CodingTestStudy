f4b460a204e27144ae4ac199cd4934d6f8e63e2d
kangwonkim <kangwon.dev@gmail.com>
2023-04-02 23:56:06 +0900

    230402 kangwonUpdate

--- Kangwon/Greedy/2023-04-02-DongJun.cpp
+++ Kangwon/Greedy/2023-04-02-DongJun.cpp
@@ -2,13 +2,23 @@
 
 int main() {
 	int size, output = 0;
-	
+
 	std::cin >> size;
 
-	int *arr = new int[size];
+	int* arr = new int[size];
 
+	for (int i = 0; i < size; i++) {
+		std::cin >> arr[i];
+	}
+
+	for (int i = size - 1; i > 0; i--) {
+		if (arr[i] <= arr[i - 1]) {
+			output += arr[i - 1] - (arr[i] - 1);
+			
+			arr[i - 1] = arr[i] - 1;
+		}
+	}
 	
-
 	std::cout << output;
 
 }
