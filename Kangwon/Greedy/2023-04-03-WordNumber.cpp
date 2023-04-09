f14a1174ccd11d8892f140c9ca0ec9260ad8fb38
kangwonkim <kangwon.dev@gmail.com>
2023-04-09 23:04:02 +0900

    230409 update code

--- Kangwon/Greedy/2023-04-03-WordNumber.cpp
+++ Kangwon/Greedy/2023-04-03-WordNumber.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 int main() {
-	int cnt;
-	std::unordered_map<char,int> hash_map;
-	std::cin>>cnt;
-	
-	for(int i =0;i<cnt;i++){
+	int cnt, result = 0;
+	std::unordered_map<char, int> hash_map;
+	std::cin >> cnt;
+
+	for (int i = 0; i < cnt; i++) {
 		std::string str;
+		std::cin >> str;
 		int num = str.length();
-		for(char c :str){
+		for (char c : str) {
 			num--;
 			auto item = hash_map.find(c);
-			if(item != hash_map.end()){
-				hash_map[c] += std::pow(10,num);
-			}else{
-				hash_map[c] = std::pow(10,num);
+			if (item != hash_map.end()) {
+				hash_map[c] += std::pow(10, num);
+			}
+			else {
+				hash_map[c] = std::pow(10, num);
 			}
 		}
 	}
+	std::vector<int> vec;
+	
+	for (auto item : hash_map) {
+		vec.push_back(item.second);
+	}
+	std::sort(vec.begin(), vec.end(),std::greater<int>());
+	
+	int num = 9;
+	for (auto item : vec) {
+		result += item * num;
+		num--;
+	}
+	std::cout << result;
 
+	return 0;
 }
