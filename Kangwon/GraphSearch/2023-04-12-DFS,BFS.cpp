00f6b3b0519a6e6fc06ece35b6d04e95e0eac34b
kangwonkim <kangwon.dev@gmail.com>
2023-04-14 01:25:56 +0900

    230414 update Code

--- Kangwon/GraphSearch/2023-04-12-DFS,BFS.cpp
+++ Kangwon/GraphSearch/2023-04-12-DFS,BFS.cpp
@@ -1,36 +1,68 @@
 #include<iostream>
-#include<vector>
+#include <queue>
 
-int main(){    
-    int point,cnt,start;
+void DFS(bool** array, bool* chk, int point, int start);
+void BFS(bool** array, bool* chk, int point, int start);
 
-    std::cin>>point>>cnt>>start;
+int main() {
+    int point, cnt, start;
 
-    bool **array = new bool*[point];
+    std::cin >> point >> cnt >> start;
 
-    for(int i =0;i<point;i++){
-        array[i] = new bool[point];
+    bool** array = new bool* [point]();
+
+    for (int i = 0; i < point; i++) {
+        array[i] = new bool[point]();
+    }
+    
+    for (int i = 0; i < cnt; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        array[a - 1][b - 1] = true;
+        array[b - 1][a - 1] = true;
     }
 
-    for(int i =0;i<cnt;i++){
-        int a,b;
-        std::cin>>a>>b;
-        array[a][b] = true;
-    }
+    bool* chk1 = new bool[point]();
+    DFS(array, chk1, point, start-1);
+    
+    std::cout << std::endl;
 
-    bool* chk = new bool[point];
-    DFS(array,chk,point,start);
+    bool* chk2 = new bool[point]();
+    BFS(array, chk2, point, start - 1);
 
     return 0;
 }
 
-int DFS(const bool **array,bool *chk,int point ,int start){
-    
-    for(int i =0;i<point;i++){
-        chk[start] = true;
-        if(array[start][i]==true&&chk[i]==false){
-            std::cout<< start;
-            DFS(array,chk,point,i);
+void DFS(bool** array, bool* chk, int point, int start) {
+    chk[start] = true;
+    std::cout << start + 1 << " ";
+
+    for (int i = 0; i < point; i++) {
+        if (array[start][i] == true && chk[i] == false) {
+            
+            DFS(array, chk, point, i);
         }
+    }
+
+}
+
+void BFS(bool** array, bool* chk, int point, int start) {
+    chk[start] = true;
+    std::cout << start + 1 << " ";
+    
+    std::queue<int> q;
+    q.push(start);
+
+    while (!q.empty())
+    {
+        for (int i = 0; i < point; i++) {
+            if (array[q.front()][i] == true && chk[i] == false) {
+                q.push(i);
+                chk[i] = true;
+                std::cout << i + 1 << " ";
+            }
+        }
+
+        q.pop();
     }
 }
