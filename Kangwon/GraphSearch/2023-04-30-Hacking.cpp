77565aaebf66e23ac6f73bffff23f38ddbf78b30
kangwonkim <kangwon.dev@gmail.com>
2023-05-03 00:21:25 +0900

    230503 update Code\

--- Kangwon/GraphSearch/2023-04-30-Hacking.cpp
+++ Kangwon/GraphSearch/2023-04-30-Hacking.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
 #include <algorithm>
+#include <queue>
 #include <vector>
 
 int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start);
+int BFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start);
 
 int main() {
 	int point, cnt;
-	std::vector<int> a;
+	std::vector<int> vec;
 	std::cin >> point >> cnt;
 
 	std::vector<std::vector<bool>> array(point, std::vector<bool>(point, false));
@@ -17,15 +19,20 @@ int main() {
 		array[b - 1][a - 1] = true;
 	}
 
-	std::vector<bool> chk(point, false);
+	int max = 0;
 
 	for (int i = 0; i < point; i++) {
-		a.push_back(DFS(array, chk, i));
+		std::vector<bool> chk(point, false);
+		int tmp = BFS(array, chk, i);
+		vec.push_back(tmp);
+		if (max < tmp) {
+			max = tmp;
+		}
 	}
-	int max = *std::max_element(a.begin(), a.end());
+
 	for (int i = 0; i < point; i++) {
-		if (a[i] == max) {
-			std::cout << i+1 << " ";
+		if (vec[i] == max) {
+			std::cout << i + 1 << " ";
 		}
 	}
 
@@ -46,6 +53,29 @@ int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int
 		}
 	}
 	result += max;
+
+	return result;
+}
+
+int BFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start)
+{
+	int result = 0;
+	chk[start] = true;
+	std::queue<int> q;
+	q.push(start);
+
+	while (!q.empty())
+	{
+		for (int i = 0; i < chk.size(); i++) {
+			if (array[q.front()][i] == true && chk[i] == false) {
+				q.push(i);
+				chk[i] = true;
+				
+			}
+		}
+		result++;
+		q.pop();
+	}
 
 	return result;
 }
