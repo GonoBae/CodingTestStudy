f4e16a3fcafa107c2fa7236c38e995c4e4f13a90
kangwonkim <kangwon.dev@gmail.com>
2023-05-06 15:56:23 +0900

    230506 update Code 해킹문제 시간초과 해결

--- Kangwon/GraphSearch/2023-04-30-Hacking.cpp
+++ Kangwon/GraphSearch/2023-04-30-Hacking.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 #include <algorithm>
 #include <queue>
 #include <vector>
 
-int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start);
-int BFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start);
+//int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start);
+int BFS(const std::vector<std::vector<int>>& array, std::vector<bool>& chk, int start);
 
 int main() {
 	int point, cnt;
 	std::vector<int> vec;
 	std::cin >> point >> cnt;
 
-	std::vector<std::vector<bool>> array(point, std::vector<bool>(point, false));
+	std::vector<std::vector<int>> array(point);
 
 	for (int i = 0; i < cnt; i++) {
 		int a, b;
 		std::cin >> a >> b;
-		array[b - 1][a - 1] = true;
+		array[b - 1].push_back(a - 1);
 	}
 
 	int max = 0;
@@ -24,6 +24,7 @@ int main() {
 	for (int i = 0; i < point; i++) {
 		std::vector<bool> chk(point, false);
 		int tmp = BFS(array, chk, i);
+		
 		vec.push_back(tmp);
 		if (max < tmp) {
 			max = tmp;
@@ -39,25 +40,25 @@ int main() {
 	return 0;
 }
 
-int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start) {
-	int result = 0, max = 0;
-	chk[start] = true;
+// int DFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start) {
+// 	int result = 0, max = 0;
+// 	chk[start] = true;
 
-	for (int i = 0; i < chk.size(); i++) {
-		if (array[start][i] == true && chk[i] == false) {
-			result++;
-			int tmp = DFS(array, chk, i);
-			if (max < tmp) {
-				max = tmp;
-			}
-		}
-	}
-	result += max;
+// 	for (int i = 0; i < chk.size(); i++) {
+// 		if (array[start][i] == true && chk[i] == false) {
+// 			result++;
+// 			int tmp = DFS(array, chk, i);
+// 			if (max < tmp) {
+// 				max = tmp;
+// 			}
+// 		}
+// 	}
+// 	result += max;
 
-	return result;
-}
+// 	return result--;
+// }
 
-int BFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int start)
+int BFS(const std::vector<std::vector<int>>& array, std::vector<bool>& chk, int start)
 {
 	int result = 0;
 	chk[start] = true;
@@ -66,11 +67,10 @@ int BFS(const std::vector<std::vector<bool>>& array, std::vector<bool>& chk, int
 
 	while (!q.empty())
 	{
-		for (int i = 0; i < chk.size(); i++) {
-			if (array[q.front()][i] == true && chk[i] == false) {
+		for (int i : array[q.front()]) {
+			if (chk[i] == false) {
 				q.push(i);
 				chk[i] = true;
-				
 			}
 		}
 		result++;
