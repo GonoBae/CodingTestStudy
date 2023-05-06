9c9728b3bb910de25954309975d0eaefb87f5bf7
kangwonkim <kangwon.dev@gmail.com>
2023-05-06 17:04:30 +0900

    230506 update Code
    섬의 개수 문제 풀이

--- Kangwon/GraphSearch/2023-04-18-NumberOfIslands.cpp
+++ Kangwon/GraphSearch/2023-04-18-NumberOfIslands.cpp
@@ -1,39 +1,90 @@
 #include <iostream>
 #include <vector>
 
-void BFS(std::vector<std::vector<bool>>& array, int col, int row);
+void DFS(std::vector<std::vector<bool>>& array, int col, int row);
 
-int main() {
-    int row, col;
-    std::cin >> col >> row;
-    std::vector<std::vector<bool>> array(col, std::vector<bool>(row, false));
+int main()
+{
+    while (true)
+    {
+        int row, col;
+        std::cin >> row >> col;
+        if (row == 0 && col == 0) break;
 
-    for (int i = 0; i < col; i++) {
-        for (int j = 0; j < row; j++) {
-            int chk;
-            std::cin >> chk;
-            array[i][j] = chk;
+        std::vector<std::vector<bool>> array(col, std::vector<bool>(row, false));
+        int result = 0;
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                int chk;
+                std::cin >> chk;
+                array[i][j] = chk;
+            }
         }
-    }
 
-    for (int i = 0; i < col; i++) {
-        for (int j = 0; j < row; j++) {
-            BFS(array, i, j);
+        for (int i = 0; i < col; i++)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                if (array[i][j])
+                {
+                    result++;
+                    DFS(array, i, j);
+                }
+            }
         }
+
+        std::cout << result << std::endl;
+
     }
 
     return 0;
 }
 
-void BFS(std::vector<std::vector<bool>>& array, int col, int row) {
-    
-    if (array[col][row]) {
+void DFS(std::vector<std::vector<bool>>& array, int col, int row)
+{
+    if (array[col][row] == true)
+    {
         array[col][row] = false;
-        if (array.size() > col+1) {
-            BFS(array, col + 1, row);
+
+        if (array[0].size() > row + 1)
+        {
+            DFS(array, col, row + 1);
         }
-        if (array[0].size() > row+1) {
-            BFS(array, col, row + 1);
+
+        if (row - 1 >= 0)
+        {
+            DFS(array, col, row - 1);
+        }
+
+        if (array.size() > col + 1)
+        {
+            DFS(array, col + 1, row);
+
+            if (array[0].size() > row + 1)
+            {
+                DFS(array, col + 1, row + 1);
+            }
+
+            if (row - 1 >= 0)
+            {
+                DFS(array, col + 1, row - 1);
+            }
+        }
+        if (col - 1 >= 0)
+        {
+            DFS(array, col - 1, row);
+
+            if (array[0].size() > row + 1)
+            {
+                DFS(array, col - 1, row + 1);
+            }
+
+            if (row - 1 >= 0)
+            {
+                DFS(array, col - 1, row - 1);
+            }
         }
     }
 }
